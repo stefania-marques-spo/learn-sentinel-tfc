bff19d08164d8cfc54b2d1034ca1f960116556df
Stefania_Marques <Stefania_Marques@mckinsey.com>
2023-06-21 17:43:57 -0300

    Third commit

--- versions.tf
+++ versions.tf
@@ -4,17 +4,6 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 4.0"
     }
-    required_version = ">= 0.15"
   }
-}
-
-  
-
-  backend "remote" {
-    organization = "<YOUR_TERRAFORM_ORG>"
-
-    workspaces {
-      name = "sentinel-example"
-    }
-  }
+}  
 
