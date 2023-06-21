0572ae41746c47159dfdfe9097317b6f7b2492dc
Stefania_Marques <Stefania_Marques@mckinsey.com>
2023-06-21 17:41:18 -0300

    Second commit

--- versions.tf
+++ versions.tf
@@ -1,16 +1,14 @@
 terraform {
   required_providers {
-    random = {
-      source  = "hashicorp/random"
-      version = "3.0.1"
-    }
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 3.26.0"
+      version = "~> 4.0"
     }
+    required_version = ">= 0.15"
   }
+}
 
-  required_version = ">= 0.15"
+  
 
   backend "remote" {
     organization = "<YOUR_TERRAFORM_ORG>"
@@ -20,4 +18,3 @@ terraform {
     }
   }
 
-}
